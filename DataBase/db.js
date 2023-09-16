60e8926a0fe724fdfef7e5ee135c28bf9cf22d4b
unknown <hamamhussein10@gmail.com>
2023-09-16 19:28:49 +0300

    To Do App

--- DataBase/db.js
+++ DataBase/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const Connection = () => {
   mongoose
-    .connect(
-      "mongodb+srv://hammamhusseinjs:UT5bwj7KpZbPYJKY@todoapp.diarjwf.mongodb.net/?retryWrites=true&w=majority"
-    )
+    .connect(process.env.DB_URL)
     .then((conn) => console.log(`connection start: ${conn.connection.host}`));
 };
 
