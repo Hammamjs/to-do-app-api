73510996331d996699c112d97f4f788a61b6d5f4
unknown <hamamhussein10@gmail.com>
2023-09-26 14:54:18 +0300

    To Do App

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 // security package
-const xssFilters = require("xss-filters");
-const mongoSanitize = require("express-mongo-sanitize");
-const { rateLimit } = require("express-rate-limit");
+const xssFilters = require('xss-filters');
+const mongoSanitize = require('express-mongo-sanitize');
+const { rateLimit } = require('express-rate-limit');
 
-const Connection = require("./DataBase/db");
-const globalErrorMiddleware = require("./Middleware/globalErrorMiddleware");
+const Connection = require('./DataBase/db');
+const globalErrorMiddleware = require('./Middleware/globalErrorMiddleware');
 
-const { mount } = require("./Routes/index");
+const { mount } = require('./Routes/index');
 
-dotenv.config({ path: "config.env" });
+dotenv.config({ path: 'config.env' });
 
 const app = express();
 // start DataBase Connection
 Connection();
 
 // Enable other domain to access application
-app.use(cors());
-app.options("*", cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.options('*', cors());
 
-app.use(express.json({ limit: "20kb" }));
-app.use(express.static(path.join(__dirname, "puplic")));
+app.use(express.json({ limit: '20kb' }));
+app.use(express.static(path.join(__dirname, 'puplic')));
 // get cookie from body
 app.use(cookieParser());
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
   console.log(`${process.env.NODE_ENV} mode start`);
 }
 // sanitize data
@@ -48,31 +52,29 @@ app.use(mongoSanitize());
 const limit = rateLimit({
   windowMs: 20 * 60 * 1000,
   limit: 10,
-  message: "To many Request from this IP please wait for couple minutes",
+  message: 'To many Request from this IP please wait for couple minutes',
 });
 // Just for authentication routes
-app.use("api/v2/auth/", limit);
+app.use('api/v2/auth/', limit);
 
 // Mount routes
 mount(app);
-app.all("*", (req, res) => {
-  res
-    .status(404)
-    .json({
-      status: "fail",
-      message: `This route not exist ${req.originalUrl}`,
-    });
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `This route not exist ${req.originalUrl}`,
+  });
 });
 app.use(globalErrorMiddleware);
 const port = process.env.PORT | 8000;
 const server = app.listen(port, () => {
-  console.log("Server start running at port: " + port);
+  console.log('Server start running at port: ' + port);
 });
 
-process.on("unhandledRejection", (err) => {
+process.on('unhandledRejection', (err) => {
   console.log(`unhandledRejection occur: ${err.name} | ${err.message}`);
   server.close(() => {
-    console.log("server shutting down");
+    console.log('server shutting down');
     process.exit(1);
   });
 });
