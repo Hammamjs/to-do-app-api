762583e9dac5599a1fe448dc538b52168e840056
unknown <hamamhussein10@gmail.com>
2023-10-02 09:48:49 +0300

    To Do App

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(
     credentials: true,
   })
 );
-app.options('*', cors());
+// app.options('*', cors());
 
 app.use(express.json({ limit: '20kb' }));
 app.use(express.static(path.join(__dirname, 'puplic')));
